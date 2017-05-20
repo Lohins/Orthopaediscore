afe429bf87b79175031fa9b6bc04b79cd6d52422
Lohins <comeonsitong@gmail.com>
2017-05-19 23:11:34 -0300

    首页

---
+++ Orthopaediscore/OrthopaediscoreTests/OrthopaediscoreTests.swift
@@ -0,0 +1,36 @@
+//
+//  OrthopaediscoreTests.swift
+//  OrthopaediscoreTests
+//
+//  Created by S.t on 2017/5/18.
+//  Copyright © 2017年 S.t. All rights reserved.
+//
+
+import XCTest
+@testable import Orthopaediscore
+
+class OrthopaediscoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Orthopaediscore/Pods/Target Support Files/Pods-Orthopaediscore/Pods-Orthopaediscore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Orthopaediscore : NSObject
+@end
+@implementation PodsDummy_Pods_Orthopaediscore
+@end
